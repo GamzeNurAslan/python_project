f1d4bd27ede676de65079735c048422459197156
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-01-04 14:03:40 +0300

    game_data.py

---
+++ Higher or Lower Project/game_data.py
@@ -0,0 +1,158 @@
+data = [
+    {
+      'name' : 'Turkey',
+      'population' : 88_844_934,
+      'description' : 'country',
+      'capital_city' : 'Ankara'
+    },
+    {
+      'name' : 'Azerbaijan',
+      'population' : 10_135_373,
+      'description' : 'country',
+      'capital_city' : 'Bakü'
+    },
+    {
+      'name' : 'ABD',
+      'population' : 336_981_386,
+      'description' : 'country',
+      'capital_city' : 'Washington, D.C.'
+    },
+    {
+      'name' : 'Russia',
+      'population' : 146_150_789,
+      'description' : 'country',
+      'capital_city' : 'Moskova'
+    },
+    {
+      'name' : 'Germany',
+      'population' : 84_607_016,
+      'description' : 'country',
+      'capital_city' : 'Berlin'
+    },
+    {
+      'name' : 'Belgium',
+      'population' : 11_808_859,
+      'description' : 'country',
+      'capital_city' : 'Brüksel'
+    },
+    {
+      'name' : 'Greece',
+      'population' : 10_482_487,
+      'description' : 'country',
+      'capital_city' : 'Atina'
+    },
+    {
+      'name' : 'Italy',
+      'population' : 58_919_345,
+      'description' : 'country',
+      'capital_city' : 'Roma'
+    },
+    {
+      'name' : 'Japan',
+      'population' : 123_850_000,
+      'description' : 'country',
+      'capital_city' : 'Tokyo'
+    },
+    {
+      'name' : 'Chinese',
+      'population' : 1_409_670_000,
+      'description' : 'country',
+      'capital_city' : 'Pekin'
+    },
+    {
+      'name' : 'England',
+      'population' : 67_026_292,
+      'description' : 'country',
+      'capital_city' : 'Londra'
+    },
+    {
+      'name' : 'India',
+      'population' : 1_427_097_903,
+      'description' : 'country',
+      'capital_city' : 'Yeni Delhi'
+    },
+    {
+      'name' : 'Canada',
+      'population' : 41_012_563,
+      'description' : 'country',
+      'capital_city' : 'Ottava'
+    },
+    {
+      'name' : 'Holland',
+      'population' : 17_947_684,
+      'description' : 'country',
+      'capital_city' : 'Amsterdam'
+    },
+    {
+      'name' : 'Kazakhstan',
+      'population' : 20_033_546,
+      'description' : 'country',
+      'capital_city' : 'Astana'
+    },
+    {
+      'name' : 'South Korea',
+      'population' : 51_439_038,
+      'description' : 'country',
+      'capital_city' : 'Seul'
+    },
+    {
+      'name' : 'Norway',
+      'population' : 5_514_042,
+      'description' : 'country',
+      'capital_city' : 'Oslo'
+    },
+    {
+      'name' : 'Portugal',
+      'population' : 10_467_366,
+      'description' : 'country',
+      'capital_city' : 'Lizbon'
+    },
+    {
+      'name' : 'Switzerland',
+      'population' : 8_865_270,
+      'description' : 'country',
+      'capital_city' : 'Bern'
+    },
+    {
+      'name' : 'Ireland',
+      'population' : 5_123_536,
+      'description' : 'country',
+      'capital_city' : 'Dublin'
+    },
+    {
+      'name' : 'Bulgaria',
+      'population' : 6_447_710,
+      'description' : 'country',
+      'capital_city' : 'Sofya'
+    },
+    {
+      'name' : 'Denmark',
+      'population' : 5_961_249,
+      'description' : 'country',
+      'capital_city' : 'Kopenhag'
+    },
+    {
+      'name' : 'Swedish',
+      'population' : 10_545_310,
+      'description' : 'country',
+      'capital_city' : 'Stockholm'
+    },
+    {
+      'name' : 'Iranian',
+      'population' : 85_231_028,
+      'description' : 'country',
+      'capital_city' : 'Tahran'
+    },
+    {
+      'name' : 'Mexican',
+      'population' : 129_713_690,
+      'description' : 'country',
+      'capital_city' : 'Mexican'
+    },
+    {
+      'name' : 'Spain',
+      'population' : 48_345_223,
+      'description' : 'country',
+      'capital_city' : 'Madrid'
+    },
+]
