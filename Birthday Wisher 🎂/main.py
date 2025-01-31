d7e0670681594bfb80f22693d7b7540359e5be70
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-01-31 13:53:13 +0300

    main.py

--- Birthday Wisher 🎂/main.py
+++ Birthday Wisher 🎂/main.py
@@ -12,20 +12,27 @@ today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,
-                            to_addrs=birthday_person["email"],
-                            msg="Subject:Mutlu Yıllar 🎉!\n\nYeni yaşın sana sağlık, mutluluk ve başarı getirsin. Her geçen gün daha da güçlenerek, hayallerine bir adım daha yaklaşmanı diliyorum. Doğum günün kutlu olsun, sevdiklerinle birlikte çok güzel bir yıl geçirmen dileğiyle".encode('utf-8'))
-     connection.close()
+    try:
+        with open(file_path) as letter_file:
+             contents = letter_file.read()
+             contents = contents.replace("[NAME]", birthday_person["name"])
+
+        with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+             connection.starttls()
+             connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+             connection.sendmail(from_addr=MY_EMAIL,
+                                 to_addrs=birthday_person["email"],
+                                 msg=f"Subject:Mutlu Yıllar 🎉\n\n{contents}"
+                            )
+             print("E-posta başarıyla gönderildi.")
+    except Exception as e:
+        print(f"Hata oluştu: {e}")
+else:
+    print("Bugün doğum günü olan kimse yok.")
 
 
-            
