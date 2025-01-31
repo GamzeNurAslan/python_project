5825e1bf77e1efdd8708f17499e5477fba6f327c
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-01-31 12:11:30 +0300

    main.py

---
+++ Monday Motivation Project 🌞/main.py
@@ -0,0 +1,26 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "your_mail"
+MY_PASSWORD = "your_password"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+        connection.close()
+
+#Pazartesi günlerine için motive edici sözlerle başla :)
