fcf57b96ed167edd49c3a8d5503ef386234e1545
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-01 15:45:54 +0300

    data.py

--- Quizler-App ✅-❌/data.py
+++ Quizler-App ✅-❌/data.py
@@ -6,6 +6,6 @@ parameters = {
 }
 
 response = requests.get("https://opentdb.com/api.php", params=parameters)
-response.raise_for_status()
+response.raise_for_status()  #4xx-5xx aralığındaki hata uyarıları verir.
 data = response.json()
 question_data = data["results"]
