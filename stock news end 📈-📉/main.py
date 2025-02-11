a129659680ad7606de484a22c352e9fd8c6f0d07
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-11 14:44:50 +0300

    main.py

--- stock news end 📈-📉/main.py
+++ stock news end 📈-📉/main.py
@@ -83,4 +83,4 @@ if abs(diff_percent) > 1:
             to=VERIFIED_NUMBER
         )
 
-#Belirli bir hisse senedinin (örneğin TSLA - Tesla Inc) fiyatında %5'ten fazla bir artış veya azalış olduğunda ilgili haberleri almanmasını ve bu haberleri Twilio kullanarak telefonunuza mesaj olarak gönderilmesini sağlamak için yazıldı :)
+#Belirli bir hisse senedinin (örneğin TSLA - Tesla Inc) fiyatında %5'ten fazla bir artış veya azalış olduğunda ilgili haberlerin alınmasını ve bu haberleri Twilio kullanarak telefonunuza mesaj olarak gönderilmesini sağlamak için yazıldı :)
